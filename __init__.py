ff5d4d1461edd6f9fe46d3e4889a4774c285bfe2
Immac <immac.gm@gmail.com>
2025-10-08 16:18:41 -0600

    Add initial project structure with configuration files, workflows, and example node implementation

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for immac_tools."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Immac"""
+__email__ = "immac.gm@gmail.com"
+__version__ = "0.0.1"
+
+from .src.immac_tools.nodes import NODE_CLASS_MAPPINGS
+from .src.immac_tools.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
